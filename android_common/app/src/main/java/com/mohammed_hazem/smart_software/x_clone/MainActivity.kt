4cc1eb3f38c4ebe54f9e8ad50d2499de739ca98d
mohammedhazem <mo.hazem2007@gmail.com>
2025-09-09 06:07:47 +0300

    Adding Blank Android Module

---
+++ android_common/app/src/main/java/com/mohammed_hazem/smart_software/x_clone/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mohammed_hazem.smart_software.x_clone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mohammed_hazem.smart_software.x_clone.ui.theme.Android_commonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android_commonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_commonTheme {
+        Greeting("Android")
+    }
+}
