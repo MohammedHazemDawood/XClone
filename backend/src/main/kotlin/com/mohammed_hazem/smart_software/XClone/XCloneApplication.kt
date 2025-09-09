007c7043ecf7c00bcbfe460f90453fb364bdd34e
mohammedhazem <mo.hazem2007@gmail.com>
2025-09-09 05:29:28 +0300

    Adding Backend Blank

---
+++ backend/src/main/kotlin/com/mohammed_hazem/smart_software/XClone/XCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.mohammed_hazem.smart_software.XClone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XCloneApplication
+
+fun main(args: Array<String>) {
+	runApplication<XCloneApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/mohammed_hazem/smart_software/XClone/XCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mohammed_hazem.smart_software.XClone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XCloneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
